6eef628d34f84db950837a62d300f4decc4b595c
Slav <exemple@gmail.com>
2021-04-04 23:56:14 +0300

    add Randomizer

---
+++ Randomizer/script.js
@@ -0,0 +1,81 @@
+
+var btnGenerate = document.querySelector(".btnGenerate");
+var btnReset = document.querySelector(".btnReset");
+var minInputText = document.querySelector(".min");
+var maxInputText = document.querySelector(".max");
+var randomTitle = document.querySelector(".random__title");
+var randomNumber = document.querySelector(".random__number");
+var fin = document.querySelector(".random__fin");
+var label = true;
+var rand;
+var addNumber;
+var range = 1;
+var count = 1;
+var arr = [];
+
+function isValid(min, max){
+	if (min <= 0 || max <= 0){
+		return false;
+	}
+
+	if(min % 1 !== 0 || max % 1 !== 0){
+		return false;
+	}
+
+	return true; 
+}
+
+function generateRandom(min, max){
+	var random =Math.floor(min + Math.random() * (max + 1 - min)); 
+	return random;
+}
+
+function getSortArr(arr){
+	for(var i = 1; i < arr.length; i++){
+		var current = arr[i];
+		for(var y = i; y > 0 && arr[y - 1] > current; y++){
+			arr[y] = arr[y - 1];
+		}
+		arr[y] = current;
+	}
+
+	return arr;
+}
+
+
+
+btnGenerate.addEventListener("click", function(e){
+	e.preventDefault();
+	if(label){
+		var min = +minInputText.value;
+		var max = +maxInputText.value;
+		if(isValid(min, max)){
+			rand = generateRandom(min, max);
+			arr.push(rand);
+			range = max - min + 1;
+			label = false;
+			minInputText.disabled = true;
+			maxInputText.disabled = true;
+		}else{
+			alert("Введены некорректные данные");
+		}
+	}else if(count < range){
+		min = +minInputText.value;
+		max = +maxInputText.value;
+		addNumber = generateRandom(min, max);
+		if(arr.indexOf(addNumber) === -1){
+			arr.push(addNumber);
+			randomNumber.innerHTML += addNumber + ", ";
+			count++;
+			console.log(arr);
+		}
+
+	}else if(count === range){
+		fin.innerHTML = "Elements are over";
+		btnGenerate.disabled = true;
+
+	}
+	
+});
+
+
